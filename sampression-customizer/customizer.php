1f08aa716808dca461ecf01b18de8358f23f5496
Pravash Karki <pravash@lastdoorsolutions.com>
2020-04-07 21:24:46 +0545

    Remove old fields

--- sampression-customizer/customizer.php
+++ sampression-customizer/customizer.php
@@ -12,8 +12,7 @@ $sampression_customizer_settings = array(
 	'social-options',
 	'blog-options',
 	'footer-options',
-	'prev-custom-css',
-	'theme-info', // new feature ->shows theme update docs url etc
+	'theme-info',
 	'sidebar-layout',
 );
 
@@ -51,7 +50,7 @@ function sampression_options_theme_option( $option_name ) {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function sampression_customize_preview_js() {
-	wp_enqueue_script( 'sampression_customizer', get_template_directory_uri() . '/sampression-customizer/js/customizer.js', array( 'jquery' ), '1.0', true );
+	wp_enqueue_script( 'sampression-customizer', get_template_directory_uri() . '/sampression-customizer/js/customizer.js', array( 'jquery' ), '1.0', true );
 }
 
 add_action( 'customize_preview_init', 'sampression_customize_preview_js' );
@@ -60,10 +59,10 @@ add_action( 'customize_preview_init', 'sampression_customize_preview_js' );
  * Enqueue Scripts for customize controls
  */
 function sampression_customize_scripts() {
-	wp_enqueue_script( 'Sampression_Customize_control', get_template_directory_uri() . '/sampression-customizer/js/customizer-control.js', array( 'jquery' ), '', true );
-	wp_enqueue_script( 'Sampression_Customize_preview', get_template_directory_uri() . '/sampression-customizer/js/customizer-preview.js', array( 'jquery' ), '', true );
-	wp_enqueue_style( 'metro_magazine-admin-style', get_template_directory_uri() . '/sampression-customizer/css/customizer-admin.css', '1.0', 'screen' );
-	wp_enqueue_script( 'metro_magazine-admin-js', get_template_directory_uri() . '/sampression-customizer/js/customizer-admin.js', array( 'jquery' ), '', true );
+	wp_enqueue_script( 'sampression-customize-control', get_template_directory_uri() . '/sampression-customizer/js/customizer-control.js', array( 'jquery' ), '', true );
+	wp_enqueue_script( 'sampression-customize-preview', get_template_directory_uri() . '/sampression-customizer/js/customizer-preview.js', array( 'jquery' ), '', true );
+	wp_enqueue_style( 'sampression-admin-style', get_template_directory_uri() . '/sampression-customizer/css/customizer-admin.css', '1.0', 'screen' );
+	wp_enqueue_script( 'sampression-admin-js', get_template_directory_uri() . '/sampression-customizer/js/customizer-admin.js', array( 'jquery' ), '', true );
 }
 
 add_action( 'customize_controls_enqueue_scripts', 'sampression_customize_scripts' );
--- sampression-customizer/inc/customizer-controls.php
+++ sampression-customizer/inc/customizer-controls.php
@@ -2,121 +2,11 @@
 
 /**
  * Contains methods for custom controls for Sampression Theme
- * 
+ *
  * @since Sampression 2.0
  */
 
 if( class_exists( 'WP_Customize_Control' ) ) :
-
-    class Sampression_CSS_Control extends WP_Customize_Control {
-
-        public $type = 'customcss';
-
-        protected function render_content() {
-
-            ?>
-            <label>
-                <?php if ( ! empty( $this->label ) ) : ?>
-                    <span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
-                <?php endif;
-                if ( ! empty( $this->description ) ) : ?>
-                    <span class="description customize-control-description"><?php echo $this->description; ?></span>
-                <?php endif; ?>
-                <textarea rows="25" <?php $this->link(); ?> style="width: 100%"><?php echo esc_textarea( $this->value() ); ?></textarea>
-            </label>
-            <?php
-        }
-    }
-
-    class Sampression_Categories_Control extends WP_Customize_Control {
-
-        public $type = 'categories';
-
-        protected function render_content() {
-            $data = array();
-            ?>
-            <div class="sampression-cat-lists">
-                <?php if ( ! empty( $this->label ) ) : ?>
-                    <span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
-                <?php endif;
-                if ( ! empty( $this->description ) ) : ?>
-                    <span class="description customize-control-description"><?php echo $this->description; ?></span>
-                <?php endif; ?>
-                <ul>
-                    <?php
-                    wp_list_categories(
-                        array(
-                            'title_li' => '',
-                            'walker' => new Sampression_Categories_Walk//( $this->value() )
-                        )
-                    );
-                    ?>
-                </ul>
-                <?php /* <textarea class="sampression-control-cat" style="width:100%;" <?php $this->link(); ?> rows="20"><?php echo $this->value(); ?></textarea> */ ?>
-                <input class="sampression-control-cat" type="hidden" <?php $this->link(); ?> value="<?php echo esc_attr( $this->value() ); ?>" />
-            </div>
-            <?php
-        }
-    }
-
-    class Sampression_Categories_Walk extends Walker_Category {
-
-        public $cats;
-
-        // function __construct( $val ) {
-        //     $this->cats = $val;
-        // }
-
-        public function start_lvl( &$output, $depth = 0, $args = array() ) {
-            $depth++;
-            $indent = str_repeat("-", $depth);
-            $output .= "";//"$indent";
-        }
-
-        public function end_lvl( &$output, $depth = 0, $args = array() ) {
-            $depth++;
-            $indent = str_repeat("-", $depth);
-            $output .= "";//"$indent";
-        }
-
-        public function start_el( &$output, $category, $depth = 0, $args = array(), $id = 0 ) {
-            $options = get_option( 'sampression_theme_options', array() );
-            if( get_theme_mod( 'categories_post_display' ) ) {
-                $value = get_theme_mod( 'categories_post_display' );
-                if(is_array($value)) {
-                    parse_str($value[0], $cat_display_count);
-                } else {
-                    if(!empty($options['category_posts_display'])) {
-                        $value = $options['category_posts_display'];
-                        parse_str($value, $cat_display_count);
-                    } else {
-                        parse_str($value, $cat_display_count);
-                    }
-                }
-                $count_value = $cat_display_count[$category->slug];
-            } else {
-                if(!empty($options['category_posts_display'])) {
-                    $value = $options['category_posts_display'];
-                    parse_str($value, $cat_display_count);
-                    $count_value = $cat_display_count[$category->slug];
-                } else {
-                    $count_value = $category->count;
-                }
-            }
-
-            $output .= "<li>";
-            $indent = str_repeat("- ", $depth);
-            $output .= '<input class="cat-checked" id="cat-'.$category->slug.'" type="checkbox" value="'.esc_attr( $category->slug ).'"'.checked( $count_value, 0, false ).'>';
-            $output .= '<label for="cat-'.$category->slug.'">'.$indent.esc_html( $category->name ).'</label>';
-            $output .= '<input type="number" value="'.$count_value.'" name="'.$category->slug.'" class="sam-number-input" max="'.$category->count.'" min="-1">';
-            //$output .= '<input type="number" value="'.$category->count.'" name="'.$category->slug.'" class="sam-number-input" max="'.$category->count.'" min="-1">';
-        }
-
-        public function end_el( &$output, $page, $depth = 0, $args = array() ) {
-            $output .= "</li>";
-        }
-
-    }
 
     class Sampression_Checkboxes_Control extends WP_Customize_Control {
 
@@ -148,26 +38,6 @@ if( class_exists( 'WP_Customize_Control' ) ) :
                 <input type="hidden" <?php $this->link(); ?> value="<?php echo esc_attr( implode( ',', $multi_values ) ); ?>" />
             </div>
             <?php
-        }
-    }
-
-    class Sampression_Fontsize_Control extends WP_Customize_Control {
-        public $type = 'range';
-
-        public function render_content() {
-        ?>
-        <label>
-            <?php if ( ! empty( $this->label ) ) : ?>
-                <span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
-            <?php endif;
-            if ( ! empty( $this->description ) ) : ?>
-                <span class="description customize-control-description"><?php echo $this->description; ?></span>
-            <?php endif; ?>
-            <input type="<?php echo esc_attr( $this->type ); ?>" <?php $this->input_attrs(); ?> value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); ?> data-reset_value="<?php echo esc_attr( $this->setting->default ); ?>" />
-            <input type="number" <?php $this->input_attrs(); ?> class="et-pb-range-input" value="<?php echo esc_attr( $this->value() ); ?>" />
-            <span class="et_divi_reset_slider"></span>
-        </label>
-        <?php
         }
     }
 
