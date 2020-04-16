a16aa97861540aba20f24f8c602ea1045316bde0
Pravash Karki <pravash@lastdoorsolutions.com>
2020-04-16 15:32:08 +0545

    Implement color option

--- sampression-customizer/inc/dynamic-css.php
+++ sampression-customizer/inc/dynamic-css.php
@@ -3,11 +3,19 @@
 function sampression_header_style() {
 	$style = '';
 
-	$primary_font_family = sampression_get_font_family( sampression_get_option( 'primary_font_family' ) );
+	// Font Family.
+	$primary_font_family   = sampression_get_font_family( sampression_get_option( 'primary_font_family' ) );
 	$secondary_font_family = sampression_get_font_family( sampression_get_option( 'secondary_font_family' ) );
 
 	$style .= sampression_generate_css( 'body,.site-title', 'font-family', wp_kses_post( $primary_font_family ) );
 	$style .= sampression_generate_css( 'h1,h2,h3,h4,h5,h6', 'font-family', wp_kses_post( $secondary_font_family ) );
+
+	// Color.
+	$primary_color   = sampression_get_option( 'primary_color' );
+	$secondary_color = sampression_get_option( 'secondary_color' );
+
+	$style .= sampression_generate_css( 'body,.site-title', 'color', esc_attr( $primary_color ) );
+	$style .= sampression_generate_css( 'h1,h2,h3,h4,h5,h6', 'color', esc_attr( $secondary_color ) );
 
 	if ( ! empty( $style ) ) {
 		echo '<style>';
